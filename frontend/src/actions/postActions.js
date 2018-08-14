888647b93cdc28de4aaf7f24a98f6309090760b7
zainali95 <zainali95@yahoo.com>
2018-08-14 19:26:24 +0500

    change package.json no sass rendering

--- frontend/src/actions/postActions.js
+++ frontend/src/actions/postActions.js
@@ -1,4 +1,9 @@
 import {ADD_TODO,FETCH_TODO }from './types';
+import {getTodos,setTodos,filterTodos} from './../Api/TodoApi';
+// var getTodos=getTodos();
 export function fetchTodo(){
-    
+    return function(dispatch){
+        type:FETCH_TODO ,
+        payload : getTodos()
+    };
 }
